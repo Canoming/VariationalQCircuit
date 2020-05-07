dc0fb5d4230ac72c89af451c80d49bf8658f44e4
canoming <canoming@163.com>
2020-05-08 03:01:44 +0800

    Includes submodules

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
-from setuptools import setup
+from setuptools import setup,find_packages
 
 setup(name='variational_circuit',
-      version='0.32',
+      version='0.33',
       description='QuTip based variational circuit',
       url='https://github.com/Canoming/VariationalQCircuit',
       author='Canoming',
       author_email='canoming@163.com',
       license='MIT',
-      packages=['variational_circuit'],
+      packages=find_packages(exclude=["*.tests","*.tests.*","tests.*","tests"]),
       install_requires=[
           'numpy',
           'scipy',
